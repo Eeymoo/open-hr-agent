f17286a26f0648103149dc426ef20010369c04e5
eeymoo <eeymoo@xmail.fun>
2026-02-05 01:28:20 +0800

    refactor(routes): remove unused idRoute handler

--- src/routes/v1/[id]/index.ts
+++
@@ -1,6 +0,0 @@
-import type { Request, Response } from 'express';
-import Result from '../../../utils/Result.js';
-
-export default function idRoute(req: Request, res: Response): void {
-  res.json(new Result({ id: req.params.id }));
-}
